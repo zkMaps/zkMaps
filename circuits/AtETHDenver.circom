fb190a4facede2d04fd325c9f93206c5cdcef5f3
blakemscurr <blakemscurr@gmail.com>
2022-02-13 12:14:52 -0700

    Remove failing longitude check

--- circuits/AtETHDenver.circom
+++ circuits/AtETHDenver.circom
@@ -25,23 +25,23 @@ template AtETHDenver() {
     lt1.in[1] <== northEastLatitude;
     lt1.out === 1;
 
-    // longitude < northEastLongitude;
-    component lt2 = LessThan(64);
-    lt2.in[0] <== longitude;
-    lt2.in[1] <== northEastLongitude;
-    lt2.out === 1;
+    // // longitude < northEastLongitude;
+    // component lt2 = LessThan(64);
+    // lt2.in[0] <== longitude;
+    // lt2.in[1] <== northEastLongitude;
+    // lt2.out === 1;
 
-    // latitude > southWestLatitude;
-    component lt3 = LessThan(64);
-    lt3.in[0] <== southWestLatitude;
-    lt3.in[1] <== latitude;
-    lt3.out === 1;
+    // // latitude > southWestLatitude;
+    // component lt3 = LessThan(64);
+    // lt3.in[0] <== southWestLatitude;
+    // lt3.in[1] <== latitude;
+    // lt3.out === 1;
 
-    // longitude > southWestLatitude;
-    component lt4 = LessThan(64);
-    lt4.in[0] <== southWestLongitude;
-    lt4.in[1] <== longitude;
-    lt4.out === 1;
+    // // longitude > southWestLatitude;
+    // component lt4 = LessThan(64);
+    // lt4.in[0] <== southWestLongitude;
+    // lt4.in[1] <== longitude;
+    // lt4.out === 1;
 }
 
 component main = AtETHDenver();
--- circuits/full_build.sh
+++ circuits/full_build.sh
@@ -10,16 +10,20 @@ rm -rf ".build/$1"
 mkdir ".build/$1"
 
 # Compile the circuits
-circom "$1.circom" --r1cs --wasm --sym --c -o ".build/$1"
+circom "$1.circom" --r1cs --wasm --sym --c -o ".build/$1" || { echo 'compilation failed' ; exit 1; }
+echo "Compilation successful"
 
 # Build the witness
-node "./.build/$1/$1_js/generate_witness.js" "./.build/$1/$1_js/$1.wasm" $2 "./.build/$1/witness.wtns"
+node "./.build/$1/$1_js/generate_witness.js" "./.build/$1/$1_js/$1.wasm" $2 "./.build/$1/witness.wtns" || { echo 'building the witness failed' ; exit 1; }
+echo "Witness built"
 
 # Build the proof
-snarkjs powersoftau prepare phase2 ptau/pot12_0001.ptau ptau/pot12_final.ptau -v
-snarkjs groth16 setup "./.build/$1/$1.r1cs" ptau/pot12_final.ptau "./.build/$1/$1_0001.zkey"
-snarkjs zkey export verificationkey "./.build/$1/$1_0001.zkey" "./.build/$1/verification_key.json"
-snarkjs groth16 prove "./.build/$1/$1_0001.zkey" "./.build/$1/witness.wtns" "./.build/$1/proof.json" "./.build/$1/public.json"
+snarkjs powersoftau prepare phase2 ptau/pot12_0001.ptau ptau/pot12_final.ptau -v || { echo 'ptau failed' ; exit 1; }
+snarkjs groth16 setup "./.build/$1/$1.r1cs" ptau/pot12_final.ptau "./.build/$1/$1_0001.zkey" || { echo 'ptau failed' ; exit 1; }
+snarkjs zkey export verificationkey "./.build/$1/$1_0001.zkey" "./.build/$1/verification_key.json" || { echo 'ptau failed' ; exit 1; }
+snarkjs groth16 prove "./.build/$1/$1_0001.zkey" "./.build/$1/witness.wtns" "./.build/$1/proof.json" "./.build/$1/public.json" || { echo 'proof generation failed' ; exit 1; }
+echo "Proof built"
 
 # Verify proof
-snarkjs groth16 verify "./.build/$1/verification_key.json" "./.build/$1/public.json" "./.build/$1/proof.json"
+snarkjs groth16 verify "./.build/$1/verification_key.json" "./.build/$1/public.json" "./.build/$1/proof.json" || { echo 'verification failed' ; exit 1; }
+echo "Verification successful"
