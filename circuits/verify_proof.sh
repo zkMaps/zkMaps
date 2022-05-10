f3a6375418994478ab0b0d8ec9b025fc4be95709
blakemscurr <blakemscurr@gmail.com>
2022-05-11 08:01:51 +1200

    Fix verify script.
    Accept an argument specifiying which circuit to verify.

--- circuits/verify_proof.sh
+++ circuits/verify_proof.sh
@@ -1,3 +1,7 @@
+#!/usr/bin/env bash
+set -e
+
 # Verify proof
-snarkjs groth16 verify "./verification_key.json" "./public.json" "./proof.json" || { echo 'verification failed' ; exit 1; }
+
+snarkjs groth16 verify ".build/$1/verification_key.json" ".build/$1/public.json" ".build/$1/proof.json" || { echo 'verification failed' ; exit 1; }
 echo "Verification successful"
