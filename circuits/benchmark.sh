5add851ce4d1adcfe04d444d4951a9726a8eca4c
blakemscurr <blakemscurr@gmail.com>
2022-09-19 09:53:08 +1200

    Add groth16 to benchmarking.

--- circuits/benchmark.sh
+++ circuits/benchmark.sh
@@ -2,7 +2,9 @@
 set -e
 
 for i in {1..1}; do
-    time snarkjs plonk prove ".build/$1/$1_0001.zkey" ".build/$1/witness.wtns" ".build/$1/proof.json" ".build/$1/public.json" || { echo 'proof generation failed' ; exit 1; }
-    snarkjs plonk verify ".build/$1/verification_key.json" ".build/$1/public.json" ".build/$1/proof.json" || { echo 'verification failed' ; exit 1; }
+    # time snarkjs plonk prove ".build/$1/$1_0001.zkey" ".build/$1/witness.wtns" ".build/$1/proof.json" ".build/$1/public.json" || { echo 'proof generation failed' ; exit 1; }
+    # snarkjs plonk verify ".build/$1/verification_key.json" ".build/$1/public.json" ".build/$1/proof.json" || { echo 'verification failed' ; exit 1; }
+    time snarkjs groth16 prove ".build/$1/$1_0001.zkey" ".build/$1/witness.wtns" ".build/$1/proof.json" ".build/$1/public.json" || { echo 'proof generation failed' ; exit 1; }
+    snarkjs groth16 verify ".build/$1/verification_key.json" ".build/$1/public.json" ".build/$1/proof.json" || { echo 'verification failed' ; exit 1; }
 done
 
